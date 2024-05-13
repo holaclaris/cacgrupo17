e26cfe9f537cdcbb292e5af3f7720f1f550a6753
Hugo Llaguno <124548588+LlagHE@users.noreply.github.com>
2024-05-13 18:16:44 -0300

    Asignamos valores a las Tarjetas con la API Random User

--- app.js
+++ app.js
@@ -27,3 +27,36 @@ const clickCruz=(e)=>{
 
 hamburguesa.addEventListener('click', clickHamburguesa);
 cruz.addEventListener('click', clickCruz);
+
+
+// Nuestros Guías
+const obtenerDatos = async () => {
+    const response = await fetch('https://randomuser.me/api/?results=4');
+    const datos = await response.json();
+    return datos;
+};
+
+const asignarValores = async (foto, nombre, nacionalidad, experiencia) => {
+    // llamada a la API Random User
+    const datos = await obtenerDatos();
+
+    // definir elementos
+    const fotoGuia = document.getElementsByClassName(foto);
+    const nombreGuia = document.getElementsByClassName(nombre);
+    const nacionalidadGuia = document.getElementsByClassName(nacionalidad);
+    const experienciaGuia = document.getElementsByClassName(experiencia);
+
+    // asignar valores
+    try {
+        for (var i = 0; i < 4; i++) {
+            fotoGuia[i].src = datos.results[i].picture.large;
+            nombreGuia[i].textContent = datos.results[i].name.first + ' ' + datos.results[i].name.last;
+            nacionalidadGuia[i].textContent += ' ' + datos.results[i].location.country;
+            experienciaGuia[i].textContent = experienciaGuia[i].textContent.replace('xx', datos.results[i].registered.age);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+asignarValores('cardImg', 'nombreGuia', 'nacionalidadGuia', 'experienciaGuia');
