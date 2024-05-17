ef6ed0b46c7c0ac6a0009e520d7846e85be8f9cc
holaclaris <mamutchiquitito@gmail.com>
2024-05-17 09:44:34 -0300

    Cambios header en movil y desktop

--- scripts/app.js
+++ scripts/app.js
@@ -13,7 +13,7 @@ const clickHamburguesa=(e)=>{
     navBar.style.top = "0vh";
     barraOcultar.style.display = "flex";
     barraOcultar.style.padding = "0px 20px";
-    contenedorTitulo.style.left ="60vw"
+    contenedorTitulo.style.left ="57vw"
 }
 
 const clickCruz=(e)=>{
